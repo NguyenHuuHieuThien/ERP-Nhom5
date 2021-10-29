bbd9fa2eedfab9895b4e50805a83bd071236e038
NguyenHuuHieuThien <78004234+NguyenHuuHieuThien@users.noreply.github.com>
2021-10-29 17:48:42 +0700

    update

--- employment/models/employee.py
+++ employment/models/employee.py
@@ -4,6 +4,9 @@ class employee(models.Model):
     _description = 'My Employee'
     _inherit = 'hr.employee'
 
+    datepartin = fields.Date('Ngày Được Nhận', default = lambda self: fields.Date.today ())
+    classeducate = fields.Many2many('classlist.educate', string="Lop dao tao", Select=True)
+    extracurricular = fields.Many2many('mylib.extracurricular', string="hoat dong tham gia", Select=True)
 
     # category_ids = fields.Many2many(
     #     'hr.employee.category', 'employee_category_rel',
